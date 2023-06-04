f48210701e6a0c08442ebbcf0e8091d0eadb0a1e
YunweiSun <122465565+YunweiSun@users.noreply.github.com>
2023-06-04 11:27:15 +0200

    Add files via upload
    Main page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardGame from './CardGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CardGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+

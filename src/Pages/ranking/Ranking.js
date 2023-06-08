d6b1ed6e7cf73e9911ed5ad1dc0fc83b7a2afd28
Camille Robinson <camille.robinson.026@gmail.com>
2023-06-08 15:52:52 +0200

    Update ranking look: table title and size

--- src/Pages/ranking/Ranking.js
+++ src/Pages/ranking/Ranking.js
@@ -1,18 +1,20 @@
 import { useTable } from "react-table";
 import { useMemo } from "react";
 
+
 import './Ranking.css';
 
 import scores from "../../Data/scores.json";
+import trophy from "../../Data/images/price/trophy.svg";
 
 export default function Main() {
-  function handleClick(msg) {
-    console.log(msg);
-  }
-
   return (
     <>
-      <h1>Ranking</h1>
+      <div className="title">
+        <img src={trophy} className="trophy"></img>
+        <h1>Ranking</h1>
+        <img src={trophy} className="trophy"></img>
+      </div>
       <div className="game">
         <Ranking />
       </div>
@@ -20,46 +22,9 @@ export default function Main() {
   )
 }
 
-
-
-function HeaderGroup({ headerGroup }) {
-  return (
-    <tr className="tableHeader">
-      {headerGroup.headers.map((column) => (
-        <Header header={column} />
-      ))}
-    </tr>
-  )
-}
-
-function Header({ header }) {
-  return (
-    <th>
-      {header.render("Header")}
-    </th>
-  )
-}
-
-function Row({ row }) {
-  return (
-    <tr>
-      {row.cells.map((cell) => (
-        <Cell cell={cell} />
-      ))}
-    </tr>
-  )
-}
-
-function Cell({ cell }) {
-  return (
-    <td>{cell.render("Cell")}</td>
-  )
-}
-
-
 function sortScores() {
   let sortedScores = scores;
-  sortedScores.sort(function (a, b) { return a.turns - b.turns })
+  sortedScores.sort(function (a, b) { return a.turns - b.turns });
   return sortedScores;
 }
 
@@ -80,6 +45,26 @@ function Ranking() {
     }
   ], []);
   const { headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  function HeaderGroup({ headerGroup }) {
+    return (
+      <tr className="tableHeader">
+        {headerGroup.headers.map((column) => (
+          <th>{column.render("Header")}</th>
+        ))}
+      </tr>
+    )
+  }
+  
+  function Row({ row }) {
+    return (
+      <tr>
+        {row.cells.map((cell) => (
+          <td>{cell.render("Cell")}</td>
+        ))}
+      </tr>
+    )
+  }
 
   return (
     <table className="ranking">

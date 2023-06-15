b452621ff3efdf190ee13303e448d2cb590548a3
Camille Robinson <camille.robinson.026@gmail.com>
2023-06-15 09:49:28 +0200

    Return to no use of Routes React components in App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
 import Home from "./Pages/home/Home.js";
-import CardGame from "./Pages/cardgame/Cardgame.js";
-import Ranking from "./Pages/ranking/Ranking.js";
 
+import "./App.css";
 
-const App = () => {
-  const [activeComponent, setActiveComponent] = useState('Home');
-
-  const handleNavigation = (componentName) => {
-    setActiveComponent(componentName);
-  };
-
+export default function Main() {
+  // The title is always there: only the center part changes (see Home.js).
   return (
-    <Router>
-      <div>
-        <nav className='navigation'>
-          <ul>
-            <li onClick={() => handleNavigation('Home')}>
-              <Link to="/">Home</Link>
-            </li>
-            <li onClick={() => handleNavigation('CardGame')}>
-              <Link to="/cardgame">Card Game</Link>
-            </li>
-            <li onClick={() => handleNavigation('Ranking')}>
-              <Link to="/ranking">Ranking</Link>
-            </li>
-            
-          </ul>
-        </nav>
-
-        <div className='home-body'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cardgame" element={<CardGame />} />
-            <Route path="/ranking" element={<Ranking />} />
-            
-          </Routes>
-        </div>
-      </div>
-    </Router>
+  <>
+    <h1>Memory Game</h1>
+    <div className="gameContainer">
+      <Home />
+    </div>
+  </>
   );
 }
-
-export default App;
